dcfe636f48373d5f5257e80af6464c35f86923c5
Laura Bedoya <lau_yuls2107@hotmail.com>
2024-08-21 13:41:18 -0500

    Este es el primer commit del proyecto

---
+++ hello.py
@@ -0,0 +1,15 @@
+print('Hola desde mi compu')
+
+
+def solution(num)
+    counter = 0
+    if num//2 = 0
+        while num > 1
+            num = num / 2
+            counter +=1
+    else num = num+1
+        while num > 1 
+            num = num / 2
+            counter +=1
+    return counter
+
