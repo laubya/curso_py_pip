1374df5f55240af5d0c32fcdcd19319d9afa43b5
Laura Bedoya <lau_yuls2107@hotmail.com>
2024-08-29 20:27:01 -0500

    Se añade y empieza uso de fastapi y uvicorn

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def get_list():
+    return [1,2,3]
+
+@app.get("/contact", response_class=HTMLResponse)
+def list():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Estoy en una página con HTML</h1>
+            <p> En algun momento aprenderé a pasarle los datos de la API</p>
+        </body>
+    </html>
+    """
 
 def run():
     store.get_categories()
