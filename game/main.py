d28d67ee2d8517eb295f48ce87ae212a6be80b5f
Jorge Andrei Hernández Rodríguez <jorgeandreyhernandez26@gmail.com>
2024-09-24 20:45:45 -0500

    recomendacion archivo game

--- game/main.py
+++ game/main.py
@@ -1,75 +1,71 @@
 import random
 
-print('Bienvenido, vamos a jugar piedra, papel o tijera')
+def display_welcome():
+    print('Bienvenido, vamos a jugar piedra, papel o tijera')
+
+def get_user_option():
+    options = ('piedra', 'papel', 'tijera')
+    user_option = input('Elige: piedra, papel o tijera => ').lower()
+    
+    while user_option not in options:
+        print('Esa opción no es válida. Inténtalo de nuevo.')
+        user_option = input('Elige: piedra, papel o tijera => ').lower()
+    
+    return user_option
 
 def elections():
-    options = ('piedra', 'papel', 'tijera')
-    
-    user_option = input('piedra, papel o tijera => ')
-    user_option = user_option.lower()
-
-    if user_option not in options:
-      print('esa opcion no es valida')
-      print(' ' * 10)
-      #continue
-      return None, None
-
-    computer_option = random.choice(options)
-
-    print('Computer option =>', computer_option)
-    print(' ' * 10)
+    user_option = get_user_option()
+    computer_option = random.choice(['piedra', 'papel', 'tijera'])
+    print(f'Opción de la computadora: {computer_option}')
     return user_option, computer_option
 
-def check_rules(user_option, computer_option, user_wins, computer_wins):
+def check_winner(user_option, computer_option):
     if user_option == computer_option:
-        print('Empate!')
-        print('*' * 10)
-    
-    elif user_option == 'piedra' and computer_option == 'tijera' or user_option == 'papel' and computer_option == 'piedra' or user_option == 'tijera' and computer_option == 'papel':
-        print(f'Ganas, {user_option} le gana a {computer_option}')
-        print('*' * 10)
-        user_wins += 1
-    
+        return 'empate', None
+
+    wins = {
+        'piedra': 'tijera',
+        'papel': 'piedra',
+        'tijera': 'papel'
+    }
+
+    if wins[user_option] == computer_option:
+        return 'usuario', user_option
     else:
-        print(f'Pierdes, {computer_option} le gana a {user_option}')
-        print('*' * 10)
-        computer_wins += 1
-
-    return user_wins, computer_wins
-
-def def_wins(user_wins, computer_wins):
-    if computer_wins == 2:
-        print('El ganador es la computadora')
-        return False
-
-    if user_wins == 2:
-        print('Eres el ganador')
-        return False
-
-    return True
+        return 'computadora', computer_option
 
 def run_game():
     computer_wins = 0
     user_wins = 0
     rounds = 1
+    
+    display_welcome()
+
     while True:
-        print(' ' * 10)
-        print('ROUND', rounds)
-        print(' ' * 10)
-    
-        print('computer_wins', computer_wins)
-        print('user_wins', user_wins)
-        print(' ' * 10)
-        rounds += 1
-    
-        user_option, computer_option = elections()
-        if user_option is None:
-            continue
-            
-        user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+        print(f'\nROUND {rounds}')
+        print(f'Victorias - Usuario: {user_wins}, Computadora: {computer_wins}')
         
-        if not def_wins(user_wins, computer_wins):
+        user_option, computer_option = elections()
+        
+        winner, winning_option = check_winner(user_option, computer_option)
+        
+        if winner == 'empate':
+            print('¡Empate!')
+        elif winner == 'usuario':
+            print(f'¡Ganas! {user_option} le gana a {computer_option}.')
+            user_wins += 1
+        else:
+            print(f'¡Pierdes! {computer_option} le gana a {user_option}.')
+            computer_wins += 1
+        
+        if user_wins == 2 or computer_wins == 2:
             break
+        
+        rounds += 1
+
+    if user_wins == 2:
+        print('¡Eres el ganador!')
+    else:
+        print('El ganador es la computadora.')
 
 run_game()
-
